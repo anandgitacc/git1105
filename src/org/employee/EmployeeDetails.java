e0887014cb2c444d6f53b79fd07ef397ea0eba63
anandgitacc <anand.akmail@gmail.com>
2023-05-11 16:17:32 +0530

    gitone

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,15 @@
+package org.employee;
+
+public class EmployeeDetails {
+	
+	// private -> access specifier
+	// void    -> return type of given method
+	// empName -> method name
+	// ()      -> arguments / input / parameter place
+	// {}      => block or body of the method
+	private void empName() {
+		// business logic or implementation details
+		System.out.println("Employee name: anand");
+	}
+
+}
